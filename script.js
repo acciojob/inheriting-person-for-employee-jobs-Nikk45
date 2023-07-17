f1e538a94a97e8d457662dbf86a44ad147d9b5ae
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-07-17 18:27:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function () {
-	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+	alert(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 }
 
 function Employee(name, age, jobTitle) {
@@ -14,15 +14,13 @@ function Employee(name, age, jobTitle) {
 }
 
 Employee.prototype.jobGreet = function () {
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+	alert(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
 }
 
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
+Employee.prototype.__proto__ = Person.prototype;
 
-
-// const employee = new Employee("Bob",30,"Manager");
-// employee.jobGreet();
+const employee = new Employee("Bob",30,"Manager");
+employee.jobGreet();
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
