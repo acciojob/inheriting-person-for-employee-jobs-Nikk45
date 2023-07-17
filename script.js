c7576dc45248940432c4b1a68cebe8d1533b578e
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-07-17 18:38:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function () {
-	alert(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 }
 
 function Employee(name, age, jobTitle) {
@@ -16,7 +16,7 @@ function Employee(name, age, jobTitle) {
 }
 
 Employee.prototype.jobGreet = function () {
-	alert(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
 }
 
 Employee.prototype.__proto__ = Person.prototype;
